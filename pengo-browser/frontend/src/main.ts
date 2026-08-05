d2d39069c787d4a95865ebdea569c31ac39d2c22
Kirill Grabar <kirill.grabar4@gmail.com>
2026-08-05 19:26:30 +0200

    feat (browser): conversion to IFrames + main.ts refactoring

--- pengo-browser/frontend/src/main.ts
+++ pengo-browser/frontend/src/main.ts
@@ -6,46 +6,57 @@ import wrongProtocol from "./pages/wrong-protocol.html?raw";
 import { resolveIcon } from "./services/icon.resolver";
 
 // document.querySelector('#app').innerHTML = `<div class="hello">Hello World</div>`;
-let currentLocation: string;
-const appBody = document.querySelector(".content");
+let currentUrl: string;
+const appBodyFrame = document.querySelector("iframe");
 document.querySelector(".search-bar-go").addEventListener("click", async () => {
-  currentLocation =
+  currentUrl =
     document.querySelector<HTMLInputElement>(".search-bar-input").value;
-  await appBodyFetch();
+  await fetchPage();
 });
 
-async function appBodyFetch() {
+function renderAsText(response: pengo.Response) {
+  const binary = atob(response.Body as unknown as string);
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  appBodyFrame.srcdoc = new TextDecoder().decode(bytes);
+  resolvePengoUrlImgs();
+}
+
+function renderAsImage(response: pengo.Response) {
+  const dataUrl = `data:${response.ContentType};base64,${response.Body}`;
+  appBodyFrame.srcdoc = `<img src="${dataUrl}">`;
+}
+
+async function resolveRequest(uri: string) {
+  const response = await PengoFetch(uri);
+  if (response.ContentType.includes("text")) {
+    renderAsText(response);
+  }
+  if (response.ContentType.includes("image")) {
+    renderAsImage(response);
+  }
+}
+
+async function fetchPage() {
   const loadingSpinner = document.querySelector(".search-bar-spinner");
-  const uri = currentLocation;
-  resolveIcon(currentLocation);
+  resolveIcon(currentUrl);
   try {
-    if (uri.includes("http")) {
-      appBody.innerHTML = wrongProtocol;
+    if (currentUrl.includes("http")) {
+      appBodyFrame.srcdoc = wrongProtocol;
       return;
     }
     if (loadingSpinner) loadingSpinner.removeAttribute("hidden");
-    const response = await PengoFetch(uri);
-    if (response.ContentType.includes("text")) {
-      const binary = atob(response.Body as unknown as string);
-      const bytes = new Uint8Array(binary.length);
-      for (let i = 0; i < binary.length; i++) {
-        bytes[i] = binary.charCodeAt(i);
-      }
-      appBody.innerHTML = new TextDecoder().decode(bytes);
-      resolvePengoUrlImgs();
-    }
-    if (response.ContentType.includes("image")) {
-      //temp solution to test images
-      const dataUrl = `data:${response.ContentType};base64,${response.Body}`;
-      appBody.innerHTML = `<img src="${dataUrl}">`;
-    }
+    resolveRequest(currentUrl);
   } catch (error) {
     console.log(error);
-    appBody.innerHTML = connectionErrorPage;
+    appBodyFrame.srcdoc = connectionErrorPage;
   } finally {
     loadingSpinner.setAttribute("hidden", "");
   }
-  document.querySelector<HTMLInputElement>(".search-bar-input").value = uri;
+  document.querySelector<HTMLInputElement>(".search-bar-input").value =
+    currentUrl;
 }
 
 //Temp pengo:// img fetcher that converts response to a base64 src.
@@ -66,14 +77,14 @@ async function resolvePengoUrlImgs() {
   }
 }
 
-appBody.addEventListener("click", async () => {
+appBodyFrame.addEventListener("click", async () => {
   const e = (event.target as Element).closest("a");
   event.preventDefault();
 
   if (e) {
     const pengoedHref = e.getAttribute("href");
-    const finalHref = new URL(pengoedHref, currentLocation);
-    currentLocation = finalHref.href;
-    await appBodyFetch();
+    const finalHref = new URL(pengoedHref, currentUrl);
+    currentUrl = finalHref.href;
+    await fetchPage();
   }
 });
